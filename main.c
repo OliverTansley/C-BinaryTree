7b01991dd6e01f2c984f55554d2a7acac2cd5107
Oliver <olivertansley@btinternet.com>
2021-08-13 15:55:34 +0100

    created tree display function and improved traversal function

--- main.c
+++ main.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <ncurses.h>
 
 /**
  * Binary Search tree
@@ -21,24 +22,107 @@ struct Tree
     struct Tree *right;
 };
 
-void traversal(struct Tree *tree, FILE *f)
+/**
+ * Helper function returns the larges of two integers
+*/
+int max(int a, int b)
 {
-    if (tree->left != NULL)
+    if (a > b)
     {
-        traversal(tree->left, f);
+        return a;
     }
-    fprintf(f, "%d", tree->data);
-    if (tree->right != NULL)
+    else
     {
-        traversal(tree->right, f);
+        return b;
     }
 }
 
-void writeTree(struct Tree *tree)
+/**
+ * @brief treeHeight returns the height of a particular tree.
+ * @param tree pointer to the tree whos height is calculated.
+ * @return the number of data nodes stored below the tree.
+*/
+int treeHeight(struct Tree *tree)
 {
-    FILE *f = fopen("tree.txt", "w");
-    traversal(tree, f);
-    fclose(f);
+    if (tree == NULL)
+    {
+        return 0;
+    }
+    else
+    {
+        return max(treeHeight(tree->left), treeHeight(tree->right)) + 1;
+    }
+}
+
+/**
+ * @brief showTree function recursively displays a tree provided that ncurses mode is established 
+ * @param x the x position that the tree will be displayed
+ * @param y the y position that the tree will be displayed
+*/
+void showTree(struct Tree *tree, int x, int y)
+{
+    if (tree->data != 0)
+    {
+        mvprintw(y, x, "%d", tree->data);
+    }
+    if (tree->left != NULL)
+    {
+        for (int i = 0; i < treeHeight(tree); i++)
+        {
+            mvprintw(y + 1 + i, x - 1 - i, "/");
+        }
+        showTree(tree->left, x - treeHeight(tree), y + treeHeight(tree));
+    }
+    if (tree->right != NULL)
+    {
+        for (int i = 0; i < treeHeight(tree); i++)
+        {
+            mvprintw(y + 1 + i, x + 1 + i, "\\");
+        }
+        showTree(tree->right, x + treeHeight(tree), y + treeHeight(tree));
+    }
+}
+
+/**
+ * @brief initialises ncurses mode and calls the showTree function
+ * @param tree pointer to the tree that will be displayed to the screen 
+*/
+void display(struct Tree *tree)
+{
+    int row, col;
+    initscr();
+    getmaxyx(stdscr, row, col);
+    mvprintw(0, 0, "Your Binary Search Tree:");
+    showTree(tree, col / 2, 2);
+    mvprintw(row - 1, col - 1, "");
+    refresh();
+    getch();
+    endwin();
+}
+
+/**
+ * @brief inOrderTraversal will either write all nodes to a file or print to stdout if a file is not passed
+ * @param f pointer to file for traversal to be written to, if equal to NULL traversal will be printed using printf
+ * @param tree pointer to tree that will be traversed
+*/
+void inOrderTraversal(struct Tree *tree, FILE *f)
+{
+    if (tree->left != NULL)
+    {
+        inOrderTraversal(tree->left, f);
+    }
+    if (f == NULL)
+    {
+        printf("%d,", tree->data);
+    }
+    else
+    {
+        fprintf(f, "%d,", tree->data);
+    }
+    if (tree->right != NULL)
+    {
+        inOrderTraversal(tree->right, f);
+    }
 }
 
 /**
@@ -167,17 +251,4 @@ struct Tree *removeNode(int toRemove, struct Tree *tree)
 
 int main()
 {
-    struct Tree *t;
-    t = Tree(3);
-    addNode(5, t);
-    addNode(4, t);
-    addNode(9, t);
-    addNode(1, t);
-    addNode(2, t);
-    addNode(18, t);
-    addNode(24, t);
-
-    removeNode(18, t);
-    removeNode(24, t);
-    removeNode(5, t);
 }
